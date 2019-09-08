88ddb164926b4fc855a4cbf90190526485889db5
neo <neo_buaa@163.com>
2019-09-08 16:42:18 +0800

    update

--- algorithms/cpp/15_NumberOf1InBinary/15_NumberOf1InBinary_practise.cpp
+++ algorithms/cpp/15_NumberOf1InBinary/15_NumberOf1InBinary_practise.cpp
@@ -5,12 +5,40 @@
 using namespace std;
 
 class Solution {
+public:
+    int NumberOf1(int n) {
+        int count = 0;
+        unsigned int flag = 1;
+        for (int i = 0; i < 32; i++) {
+            if (n & flag)
+                count++;
+            flag = flag << 1;
+        }
+        return count;
+    }
+};
 
+class Solution1 {
+public:
+    int NumberOf1(int n) {
+        int count = 0;
+        unsigned int flag = 1;
+        while (flag) {
+            if (n & flag)
+                count++;
+            flag = flag << 1;
+        }
+        return count;
+    }
 };
 
 int main() {
-    auto *so = new Solution();
-    vector<int> nums{};
+//    2019-09-08 16:34:03
+//2019-09-08 16:35:43
+    auto *so = new Solution1();
+    int n = 9;
+    int res = so->NumberOf1(n);
+    print(res);
     delete so;
     return 0;
 }
