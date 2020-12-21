b6a44b6fa6cfe526213530e270a5ea90ec490273
catalina85 <catalina85@163.com>
2020-12-21 10:36:42 +0800

    coding interview lib config

---
+++ algorithms/cpp/00_utils_test/myVTKUtilsTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <myUtils.h>
+#include <myListNode.h>
+
+#include <vtkUtils.h>
+
+//using namespace std;
+
+int main() {
+    std::string name  = "SimplePointsWriter_hahaha.xyz";
+    myvtkSimplePointsWriter(name);
+    return 0;
+}
